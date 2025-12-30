8150df781ff45d1528f0d80cac5bbe5cea12931f
Tanush0907 <tanushp0905@gmail.com>
2025-12-30 21:47:20 +0530

    Added door lock sound effect

--- Assets/Scripts/DoorLock.cs
+++ Assets/Scripts/DoorLock.cs
@@ -6,6 +6,8 @@ public class DoorLock : MonoBehaviour, ISecondaryInteractable
     private bool canLock;
     public event Action OnDoorLock;
     [SerializeField] private Door door;
+    [SerializeField] private AudioSource lockSound;
+
 
     public void Interact()
     {
@@ -13,6 +15,7 @@ public class DoorLock : MonoBehaviour, ISecondaryInteractable
             door.Disable();
             canLock = false;
             OnDoorLock?.Invoke();
+            if (lockSound != null) lockSound.Play();
         }
     }
     public void SetCanLock(bool canLock)
