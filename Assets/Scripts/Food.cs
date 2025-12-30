353d210d56eb9a8a0cbb3f485b16a27ad8be226e
Tanush0907 <tanushp0905@gmail.com>
2025-12-30 20:27:22 +0530

    Refine eating interaction with sound effects and improved bowl/food visuals

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -5,6 +5,8 @@ public class Food : MonoBehaviour
 {
     [SerializeField] private float eatDuration;
     [SerializeField] private string actionName;
+    [SerializeField] private AudioSource eatSound;
+    [SerializeField] private GameObject fries;
 
     public void Eat()
     {
@@ -14,9 +16,12 @@ public class Food : MonoBehaviour
     {
         GameEvents.OnRequestProgressBar?.Invoke(eatDuration, actionName);
         GameEvents.RaiseFoodEatStart();
+
+        eatSound.Play();
+
         yield return new WaitForSeconds(eatDuration);
 
+        fries.SetActive(false);
         GameEvents.RaiseFoodEaten();
-        Debug.Log("Ate Food");
     }
 }
